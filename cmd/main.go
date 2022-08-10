b7a1676a2f822eb6466322296c882ef20b3e4dd7
kptp <zealotkid711@gmail.com>
2022-08-11 05:48:35 +0700

    Update main.go

--- cmd/main.go
+++ cmd/main.go
@@ -43,7 +43,7 @@ func main() {
 	grpcServer := grpc.NewServer()
 	pb.RegisterArithmeticServiceServer(grpcServer, arithmeticServiceServer)
 
-	log.Println("listening on port 9000")
+	log.Println("listening on port 50051")
 
 	if err := grpcServer.Serve(listen); err != nil {
 		log.Fatalf("failed to serve gRPC server over on port 9000: %v", err)
